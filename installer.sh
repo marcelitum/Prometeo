43882c7a1e5c45d532c87d8974c40b586918fdfb
marcelitum <marcelitum@users.noreply.github.com>
2015-08-03 08:38:43 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,8 @@
 # Pendiente
+#mkdir /home/$USER/.Prometeo
+#gcc PromeCli PromeCli.c
+#cp PromeCli /home/$USER/PromeCli
+#cp PromeSer /home/$USER/PromeSer
+#apt-get install ssmtp opnessh-server openssh-client samba sshfs
+
+echo Ya puede borrar estos archivos de instalacion ?
